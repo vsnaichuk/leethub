259ea14eb2fb02420dfa5874cc63e38e6ad36240
Volodymyr Snaichuk <v.snaichuk@gmail.com>
2024-08-13 23:41:08 +0200

    Time: 46 ms (94.82%), Space: 52.3 MB (43.47%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -1,18 +1,19 @@
 function search(nums, target) {
-    let lo = 0;
-    let hi = nums.length;
+    let lo = 0
+    let hi = nums.length
     
     do {
         const m = Math.floor(lo + (hi - lo) / 2)
         const v = nums[m]
         
-        if (v === target) {
+        if (target === v) {
             return m
-        } else if (v > target) {
-            hi = m
-        } else {
+        } else if (target > v) {
             lo = m + 1
+        } else {
+            hi = m
         }
+        
     } while (lo < hi)
         
     return -1
