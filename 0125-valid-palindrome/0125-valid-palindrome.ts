d616081f0897ee4c978ec812071b2959788a2186
Volodymyr Snaichuk <v.snaichuk@gmail.com>
2024-08-16 19:24:38 +0200

    Time: 75 ms (30.50%), Space: 56 MB (29.44%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.ts
+++ 0125-valid-palindrome/0125-valid-palindrome.ts
@@ -1,3 +1,8 @@
+function alphanum(c: string) {
+    return ('a'.charAt(0) <= c.charAt(0) && c.charAt(0) <= 'z'.charAt(0))
+            || ('0'.charAt(0) <= c.charAt(0) && c.charAt(0) <= '9'.charAt(0))
+}
+
 function reverseStr(s: string) {
     let res = ""
     for (let i = s.length - 1; i >= 0; i--) {
@@ -6,7 +11,12 @@ function reverseStr(s: string) {
     return res;
 }
 
-function isPalindrome(s: string): boolean {
-  const chars = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
-  return chars === reverseStr(chars)
-};
+function isPalindrome(s) {
+  let newStr = '';
+
+  for (const c of s.toLowerCase()) {
+    newStr += alphanum(c) ? c : "";
+  }
+
+  return newStr === reverseStr(newStr);
+}
