96e8e764d45dfd421ad50831eb74fe44a1e3e728
Volodymyr Snaichuk <v.snaichuk@gmail.com>
2024-08-16 18:47:39 +0200

    Time: 61 ms (84.97%), Space: 54.7 MB (38.20%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.ts
+++ 0125-valid-palindrome/0125-valid-palindrome.ts
@@ -1,4 +1,12 @@
+function reverseStr(s: string) {
+    let res = ""
+    for (let i = s.length - 1; i >= 0; i--) {
+        res += s[i];            
+    }
+    return res;
+}
+
 function isPalindrome(s: string): boolean {
   const chars = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
-  return chars === [...chars].reverse().join('')
+  return chars === reverseStr(chars)
 };
