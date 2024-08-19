60abf109ab80a2ea39ce6760e56fe0c3cbce2016
Volodymyr Snaichuk <v.snaichuk@gmail.com>
2024-08-19 10:13:15 +0200

    Time: 69 ms (12.12%), Space: 51.7 MB (29.16%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.ts
+++ 0075-sort-colors/0075-sort-colors.ts
@@ -5,7 +5,9 @@ function sortColors(nums: number[]): void {
     for (let i = 0; i < nums.length; ++i) {
         for (let k = 0; k < nums.length - 1 - i; ++k) {
             if (nums[k] > nums[k+1]) {
-                [nums[k], nums[k+1]] = [nums[k+1], nums[k]]
+                const temp = nums[k]
+                nums[k] = nums[k+1]
+                nums[k+1] = temp
             }
         }   
     }    
