d15443fd447652d3e33eb9f44919823f991a02f9
Volodymyr Snaichuk <v.snaichuk@gmail.com>
2024-08-19 10:09:21 +0200

    Time: 51 ms (86.42%), Space: 52.1 MB (10.52%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.ts
@@ -0,0 +1,12 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function sortColors(nums: number[]): void {
+    for (let i = 0; i < nums.length; ++i) {
+        for (let k = 0; k < nums.length - 1 - i; ++k) {
+            if (nums[k] > nums[k+1]) {
+                [nums[k], nums[k+1]] = [nums[k+1], nums[k]]
+            }
+        }   
+    }    
+};
