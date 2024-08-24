0dad0b90fee7cd6b18c53089f309224e69685ada
Volodymyr Snaichuk <v.snaichuk@gmail.com>
2024-08-24 12:52:22 +0200

    Time: 65 ms (48.28%), Space: 51.9 MB (73.49%) - LeetHub

--- 0202-happy-number/0202-happy-number.ts
+++ 0202-happy-number/0202-happy-number.ts
@@ -1,20 +1,21 @@
-function sumOfSquares(n) {
-    let res = 0;
-    while (n) {
-        res += (n % 10) ** 2;
-        n = Math.floor(n / 10);
-    }
-    return res;
-}
-
+// tortoise and hare algorithm
 function isHappy(n) {
     let slow = n;
-    let fast = sumOfSquares(n);
-
+    let fast = sumOfNumb(n)
+    
     while (slow !== fast) {
-        slow = sumOfSquares(slow);
-        fast = sumOfSquares(sumOfSquares(fast));
+        slow = sumOfNumb(slow);
+        fast = sumOfNumb(sumOfNumb(fast))        
     }
+    
+    return slow === 1
+}
 
-    return slow === 1;
+function sumOfNumb(numb) {
+    let sum = 0;
+    while (numb) {
+        sum += (numb % 10) ** 2
+        numb = Math.floor(numb / 10)
+    }
+    return sum
 }
