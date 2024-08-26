6ad60b0378a912c6174cf813be3c959fb5c85828
Volodymyr Snaichuk <v.snaichuk@gmail.com>
2024-08-26 15:32:29 +0200

    Time: 88 ms (63.29%), Space: 53.4 MB (98.65%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.ts
@@ -0,0 +1,14 @@
+function lengthOfLongestSubstring(s: string): number {
+    const hashMap = new Map();
+    let maxLength = 0;
+    let left = 0;
+    
+    for (let right = 0; right < s.length; right++) {
+        if (hashMap.get(s[right])) {
+            left = Math.max(hashMap.get(s[right]), left);
+        }
+        maxLength = Math.max(maxLength, right - left + 1);
+        hashMap.set(s[right], right + 1);
+    }
+    return maxLength;
+};
