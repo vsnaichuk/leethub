8837daff6618ba5d8eb4701a8a3b3a985dbe81a2
Volodymyr Snaichuk <v.snaichuk@gmail.com>
2024-08-29 16:02:05 +0200

    Time: 100 ms (26.01%), Space: 58.3 MB (89.86%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.ts
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.ts
@@ -1,11 +1,18 @@
+// [4,3,2,7,8,2,3,1]
+
 function findDisappearedNumbers(nums: number[]): number[] {
-    const numsSet = new Set(nums);
-    const res = []
+    for (const n of nums) {
+        const idx = Math.abs(n) - 1; // idx based on value (convert value to index)
+        
+        if (nums[idx] > 0)
+            nums[idx] = -1 * nums[idx] // mark as negative to indicate that number                                            // corresponding to this index has been found 
+    }
     
-    for (let i = 1; i <= nums.length; i++) {
-        if (!numsSet.has(i)) {
-            res.push(i)
-        }
+    const res = [];
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > 0) {
+            res.push(i + 1) // add +1 since arrays 0-based
+        } 
     }
     return res;
 };
