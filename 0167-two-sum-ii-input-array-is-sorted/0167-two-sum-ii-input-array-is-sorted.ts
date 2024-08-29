d48b60bc2e6d4e0013ab335aea77ff6b93d5389c
Volodymyr Snaichuk <v.snaichuk@gmail.com>
2024-08-29 14:30:33 +0200

    Time: 52 ms (93.12%), Space: 51.6 MB (42.47%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.ts
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.ts
@@ -1,12 +1,16 @@
 function twoSum(numbers: number[], target: number): number[] {
-    let l = 0, r = numbers.length - 1;
+    let l = 0, r = numbers.length - 1
     
     while (l < r) {
         const sum = numbers[l] + numbers[r]
-        if (sum > target) r--;
-        else if (sum < target) l++;
-        else return [l+1, r+1]
+        
+        if (sum > target) {
+            r--
+        } else if (sum < target) {
+            l++
+        } else {
+            return [l + 1, r + 1]
+        }
     }
-    
-    return [];
+    return []
 };
