d5bbde37c6757209aabef9c6ee774f1020942596
Volodymyr Snaichuk <v.snaichuk@gmail.com>
2024-09-06 19:17:11 +0200

    Time: 69 ms (6.39%), Space: 50.2 MB (47.53%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.ts
+++ 0070-climbing-stairs/0070-climbing-stairs.ts
@@ -1,9 +1,9 @@
 function climbStairs(n: number): number {
-    let one = 0, two = 1;
-    for (let i = 0; i <= n; i++) {
-      let temp = one
-      one = temp + two;
-      two = temp
+    let a = 1, b = 1;
+    for (let i = 0; i < n; i++) {
+        const temp = a;
+        a = a + b;
+        b = temp;
     }
-    return one;
+    return b;
 };
