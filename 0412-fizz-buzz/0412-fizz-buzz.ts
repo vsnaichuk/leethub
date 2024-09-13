dbe971279b3f5a1f8a1271674eb7d8aafac077a3
Volodymyr Snaichuk <v.snaichuk@gmail.com>
2024-09-13 15:53:44 +0200

    Time: 67 ms (38.81%), Space: 53.4 MB (14.57%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.ts
@@ -0,0 +1,10 @@
+function fizzBuzz(n: number): string[] {
+    const res = [];
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) res.push("FizzBuzz");
+        else if (i % 3 === 0) res.push("Fizz");
+        else if (i % 5 === 0) res.push("Buzz");
+        else res.push(String(i))
+    }
+    return res;
+};
