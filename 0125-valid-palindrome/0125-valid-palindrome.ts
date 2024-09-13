34f67f8a57f6bca5337ab86e648c47abb04709bb
Volodymyr Snaichuk <v.snaichuk@gmail.com>
2024-09-13 15:24:50 +0200

    Time: 80 ms (15.77%), Space: 55.2 MB (34.49%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.ts
+++ 0125-valid-palindrome/0125-valid-palindrome.ts
@@ -1,16 +1,19 @@
 function isalnum(c: string) {
-    const cc = c.charCodeAt(0);
-    return (cc >= 'A'.charCodeAt(0) && cc <= 'Z'.charCodeAt(0))||
-            (cc >= 'a'.charCodeAt(0) && cc <= 'z'.charCodeAt(0)) || 
-            (cc >= '0'.charCodeAt(0) && cc <= '9'.charCodeAt(0))
+    const charCode = c.charCodeAt(0) 
+    return (
+        'a'.charCodeAt(0) <= charCode && charCode <= 'z'.charCodeAt(0) ||
+        'A'.charCodeAt(0) <= charCode && charCode <= 'Z'.charCodeAt(0) ||
+        '0'.charCodeAt(0) <= charCode && charCode <= '9'.charCodeAt(0)
+    )
 }
 
 function isPalindrome(s: string): boolean {
-    let l = 0, r = s.length - 1;
+    const n = s.length;
     
+    let l = 0, r = n - 1;
     while (l < r) {
         while (l < r && !isalnum(s[l])) l++;
-        while (r > l && !isalnum(s[r])) r--;
+        while (l < r && !isalnum(s[r])) r--
         
         if (s[l].toLowerCase() !== s[r].toLowerCase()) {
             return false
