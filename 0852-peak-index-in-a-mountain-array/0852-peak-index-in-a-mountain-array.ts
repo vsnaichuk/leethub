d2e339d025cf832e0589532f0d597fba01544051
Volodymyr Snaichuk <v.snaichuk@gmail.com>
2024-09-14 21:06:06 +0200

    Time: 75 ms (38.54%), Space: 60.8 MB (26.04%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.ts
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.ts
@@ -1,12 +1,12 @@
 function peakIndexInMountainArray(arr: number[]): number {
-    for (let i = 1; i < arr.length - 1; i++) {
+    const n = arr.length
+    for (let i = 1; i < n - 1; i++) {
         const curr = arr[i];
         const prev = arr[i-1]
         const next = arr[i+1]
         
-        if (curr > prev && next < curr) {
-            return i
-        }
+        if (curr > prev && next < curr)
+            return i;
     }
     return -1
 };
