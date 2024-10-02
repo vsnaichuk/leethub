9b519fe10b81bfdba3d3c505462d852f6b700d83
Volodymyr Snaichuk <v.snaichuk@gmail.com>
2024-10-02 17:42:26 +0200

    Time: 76 ms (47.38%), Space: 60.5 MB (7.71%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.ts
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.ts
@@ -1,24 +1,23 @@
 function topKFrequent(nums: number[], k: number): number[] {
+    const n = nums.length;
     const map = new Map();
-    
-    for (const n of nums) {
-        map.set(n, map.has(n) ? map.get(n) + 1 : 1)
-    }
-    
-    const freq = new Array(nums.length + 1).fill(0).map(() => []);
+    const freq = new Array(n + 1).fill(0).map(() => []); // [[],[],...]
     const res: number[] = [];
     
-    for (const [n, count] of map.entries()) {
-        freq[count].push(n)
+    for (let n of nums) {
+        map.set(n, (map.get(n) || 0) + 1);
     }
     
-    for (let i = nums.length; i > 0; i--) {
-        for (const n of freq[i]) {
-            res.push(n)
-            if (res.length === k) 
+    for (let [num, count] of map.entries()) {
+        freq[count].push(num);
+    }
+    
+    for (let i = n; i > 0; i--) {
+        for (let num of freq[i]) {
+            res.push(num)
+            if (res.length === k) {
                 return res
+            }
         }
     }
-    
-    return [];
 };
