09514b67593ea2a51f16dfb5ceab94e2a07d4c54
Volodymyr Snaichuk <v.snaichuk@gmail.com>
2024-10-16 16:20:16 +0200

    Time: 69 ms (78.16%), Space: 53.3 MB (79.88%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.ts
+++ 0242-valid-anagram/0242-valid-anagram.ts
@@ -1,21 +1,22 @@
 function isAnagram(s: string, t: string): boolean {
     if (s.length !== t.length) {
-        return false;
+        return false
     }
-    
+
     const map = new Map();
-    
-    for (const char of s) {
-        map.set(char, (map.get(char) || 0) + 1)
+
+    for (const n of s) {
+        map.set(n, (map.get(n) || 0) + 1)
     }
-    
-    for (const char of t) {
-        if (!map.has(char)) {
-            return false;
-        }
-        map.set(char, map.get(char) - 1)
-        if (map.get(char) === 0) {
-            map.delete(char)
+
+    for (const n of t) {
+        if (map.has(n)) {
+            map.set(n, map.get(n) - 1)
+            if (map.get(n) === 0) {
+                map.delete(n)
+            }
+        } else {
+            return false
         }
     }
     return true;
