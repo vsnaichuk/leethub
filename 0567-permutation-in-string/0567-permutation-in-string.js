bf6ae30476e528f3e99de6531c322711fa6eba40
Volodymyr Snaichuk <v.snaichuk@gmail.com>
2024-12-01 23:14:12 +0100

    Time: 21 ms (54.3%), Space: 54.7 MB (41.78%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var checkInclusion = function (s1, s2) {
+	if (s1.length > s2.length) return false;
+	let neededChar = {};
+	for (let i = 0; i < s1.length; i++) {
+		neededChar[s1[i]] = (neededChar[s1[i]] || 0) + 1;
+	}
+
+	let left = 0,
+		right = 0,
+		requiredLength = s1.length; 
+
+	while (right < s2.length) {
+		if (neededChar[s2[right]] > 0) requiredLength--;
+		neededChar[s2[right]]--;
+		right++ 
+
+		if (requiredLength === 0) return true;
+
+		if (right - left === s1.length) {
+			if (neededChar[s2[left]] >= 0) requiredLength++;
+			neededChar[s2[left]]++;
+			left++
+		}
+	}
+	return false;
+};
