257ff95308f43deeb75ce13d4d97d9c038a35361
Volodymyr Snaichuk <v.snaichuk@gmail.com>
2025-01-25 19:51:03 +0100

    Time: 6 ms (73.17%), Space: 57.9 MB (90.66%) - LeetHub

---
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDuplicates = function(nums) {
+    const res = [];
+    const n = nums.length;
+    for (let i = 0; i < n; i++) {
+        const x = Math.abs(nums[i]);
+        if (nums[x - 1] < 0) {
+            res.push(x);
+        }
+        nums[x - 1] *= -1;
+    }
+    return res;
+}
