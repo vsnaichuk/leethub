e7ed8a6e32385df557d61dded0597b0f583136b6
Volodymyr Snaichuk <v.snaichuk@gmail.com>
2025-02-08 22:38:36 +0100

    Time: 43 ms (22.22%), Space: 52.6 MB (55.56%) - LeetHub

---
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.ts
@@ -0,0 +1,34 @@
+function findNumberOfLIS(nums: number[]): number {
+    if (nums === null || nums.length === 0) {
+        return 0;
+    }
+    let dp = new Array(nums.length);
+    let freq = new Array(nums.length);
+    for (let i = 0; i < nums.length; i++) {
+        let lenOfLIS = 1;
+        for (let j = 0; j < i; j++) {
+            if (nums[j] < nums[i]) {
+                lenOfLIS = Math.max(lenOfLIS, dp[j] + 1);
+            }
+        }
+        
+        let freqAtI = 0
+        for (let j = 0; j < i; j++) {
+            if (nums[j] < nums[i] && dp[j] + 1 === lenOfLIS) {
+                freqAtI += freq[j];
+            }
+        }
+        
+        dp[i] = lenOfLIS;
+        freq[i] = freqAtI === 0 ? 1 : freqAtI;
+    }
+
+    let maxLen = Math.max(...dp);
+    let maxFreq = 0;
+    for (let i = 0; i < dp.length; i++) {
+        if (dp[i] === maxLen) {
+            maxFreq += freq[i];
+        }
+    }
+    return maxFreq;
+};
