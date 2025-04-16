3d1a37286800874233ad20fc6d97550571ba4d1e
Volodymyr Snaichuk <v.snaichuk@gmail.com>
2025-04-16 22:14:36 +0200

    Time: 5 ms (34.11%), Space: 60.4 MB (7.29%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.ts
@@ -0,0 +1,15 @@
+function maxProduct(nums: number[]): number {
+    let max = nums[0];
+    let prevMax = nums[0];
+    let prevMin = nums[0];
+    
+    for (let i = 1; i < nums.length; i++) {
+        const options = [nums[i], nums[i] * prevMax, nums[i] * prevMin];
+        prevMax = Math.max(...options);
+        prevMin = Math.min(...options);
+        
+        max = Math.max(max, prevMax);
+    }
+    
+    return max;
+};
