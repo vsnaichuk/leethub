011e413b9ec5987d2df41776c1f1fe9d471fb54e
Volodymyr Snaichuk <v.snaichuk@gmail.com>
2025-04-18 23:05:39 +0200

    Time: 0 ms (100%), Space: 57.4 MB (67.7%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.ts
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function rightSideView(root: TreeNode | null): number[] {
+    const result = [];
+    const queue = [];
+    
+    if (root === null) return result;
+    
+    queue.push(root);
+    while(queue.length !== 0) {
+        let size = queue.length;
+        for (let i = 0; i < size; i++) {
+            let n = queue.shift();
+            if (i === size - 1) result.push(n.val);
+            if (n.left !== null) queue.push(n.left);
+            if (n.right !== null) queue.push(n.right);
+        }
+    }
+    
+    return result;
+};
