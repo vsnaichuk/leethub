97b289511dc32f0ce9900d375259a6ce5474dd0a
Volodymyr Snaichuk <v.snaichuk@gmail.com>
2025-09-03 15:55:33 +0200

    Time: 12 ms (21.63%), Space: 66.2 MB (7.72%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.ts
+++ 0009-palindrome-number/0009-palindrome-number.ts
@@ -1,15 +1,3 @@
 function isPalindrome(x: number): boolean {
-    if (x < 0) return false;
-    if (x >= 0 && x < 10) return true;
-    
-    const str = String(x)
-    let l = 0, r = str.length - 1;
-    
-    while (l < r) {
-        if (str[l] !== str[r]) 
-            return false;
-        l++;
-        r--;
-    }
-    return true
+  return x === Number(x.toString().split("").reverse().join(""));  
 };
